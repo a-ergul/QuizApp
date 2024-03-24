e33301f3fab05f654747515f8750b4c285eaeaf8
aergul <alpyergul@gmail.com>
2024-03-25 01:20:32 +0300

    Fetch Info and Questions

--- QuizApp/View/Home.swift
+++ QuizApp/View/Home.swift
@@ -2,15 +2,56 @@
 //  Home.swift
 //  QuizApp
 //
-//  Created by Alpay Ergül on 24.03.2024.
+//  Created by Alpay Ergül on 25.03.2024.
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestoreSwift
 
 struct Home: View {
+    @State private var quizInfo: Info?
+    
+    /// - Anonymous User Log
+    @AppStorage("log_status") private var logStatus: Bool = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if let info = quizInfo {
+            
+        } else {
+            VStack(spacing: 20) {
+                ProgressView()
+                Text("Please Wait")
+                    .font(.title)
+                    .foregroundColor(.gray)
+            }
+            .task {
+                do {
+                    try await fetchData()
+                } catch {
+                    print(error.localizedDescription)
+                }
+            }
+        }
     }
+    
+    ///  Fetch Info and Quiz Questions
+    func fetchData()async throws{
+        try await loginUserAnonymous()
+        let info = try await  Firestore.firestore().collection("Quiz").document("Info").getDocument().data(as: Info.self)
+        let questions = try await Firestore.firestore().collection("Quiz").document("Info").collection("Questions").getDocuments()
+            .documents
+            .compactMap{
+                try $0.data(as: Question.self)
+            }
+    }
+    /// Login User as Anonymous - Firestore
+    func loginUserAnonymous()async throws{
+        if !logStatus{
+            try await Auth.auth().signInAnonymously()
+        }
+    }
+    
 }
 
 #Preview {
